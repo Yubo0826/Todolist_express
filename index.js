c4caab78b6368deba41337dade5d5493c6d287d9
Yubo0826 <ujhy669917@gmail.com>
2021-08-03 06:44:32 +0800

    update some function"

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const firebase = require('firebase');
 // 初始一個 express服務：app
 const app = express();
 
-app.use(express.urlencoded({
+app.use(express.urlencoded({  
     extended: false
     }));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 
 firebase.initializeApp({
     databaseURL: "https://todoapp-3d73a-default-rtdb.firebaseio.com/"
 });
+
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
 
 // 設定ejs為樣版引擎以及設定讀取的資料夾為根目錄的views資料夾，app.set(name, value)
 app.engine('ejs', engine);
@@ -24,10 +32,16 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     firebase.database().ref('todos').once('value', function(snapshot) {
         var data = snapshot.val(); 
+        var itemTotal = Object.keys(data).length;
         console.log(data);
-        res.render('index', {
-            'todolist': data
-        });   
+        if(data != null){
+            res.render('index',{
+                'todoList': data
+            })
+        }else{
+            res.render('index')
+        }
+        
     });
     
     // 測試資料庫是否能使用
@@ -35,27 +49,23 @@ app.get('/', (req, res) => {
     // console.log(db);
 });
 
+// 創造物件
 app.post('/create-item', (req, res) => {
     // 利用req.body.item的方式，將從input欄位取得的值存入這個變數。
     console.log(req.body);  // ex: { item: '888' }
     var items = req.body.item;
     // itemRef變數設定資料庫的路徑，把路徑設成'todos'，並且用push方法新增資料到指定的資料庫路徑中
-    var itemRef = firebase.database().ref('todos').push();
-    itemRef.set({
-        item : items
+    var itemRef = firebase.database().ref('todos');
+    // set方法會覆蓋原有資料；push方法會在原有資料新增新資料
+    itemRef.push({
+        completed: false,
+        item: items
         }).then(function() {
-            // firebase.database().ref('todos').once('value', function(snapshot) {
-            //     res.send({
-            //         "success": true,
-            //         "result": snapshot.val(),
-            //         "message": "資料讀取成功"
-            //         });
-
-            // })
             res.redirect('/');
         });
 })
 
+//更新物件
 app.post('/update-item', function (req, res) {
     console.log(req.body);
     var id = req.body.id;   // req.body.id 就是browser.js裡面axios所post的部分
@@ -67,6 +77,17 @@ app.post('/update-item', function (req, res) {
 
 })
 
+// 更改任務物件 completed 的值
+app.post('/change-item-status', (req, res) => {
+    var id = req.body.id;
+    var status = req.body.status;
+    var dbref = firebase.database().ref('todos/' + id);
+    dbref.update({
+        completed: status
+    })
+})
+
+// 刪除物件
 app.post('/delete-item', function(req, res) {
     var id = req.body.id;
     var dbref = firebase.database().ref('todos/' + id);
--- public/browser.js
+++ public/browser.js
@@ -1,43 +1,42 @@
-// e = envent
 document.addEventListener('click', function(e) { 
     // delete feature
     if(e.target.classList.contains('delete-me')) {
+        e.target.parentElement.remove();
         let _id = e.target.getAttribute('data-id');
-        if(confirm('確定要刪除這筆資料?')) {
+        if (confirm("確定要刪除這筆資料嗎 ?")) {
             axios.post('/delete-item', {
                 id: _id
-            }).then(function() {
-                e.target.parentElement.parentElement.remove();
-                console.log('已刪除');
-            }).catch(err => {  
-                console.log(err);
             })
         }
     }
 
     // update feature ver.2 點擊文本(text)進入文本框(input)就可以修改內容
     if(e.target.classList.contains('item-text')) {
-        let originText = e.target.innerHTML;
-        let _id = e.target.getAttribute('data-id');
-        e.target.innerHTML = '<input type="text" id="updateInput"></input>';
-        var updateInput = document.getElementById('updateInput');
-        // 點擊文本框，立刻獲得焦點
-        updateInput.focus();
-        updateInput.onblur = function () {
-            let updateVaule = updateInput.value;
-            if(updateVaule != "") {
-                axios.post('/update-item', {
-                    text: updateVaule,
-                    id: _id
-                }).then(function() {
-                    e.target.innerHTML = updateVaule;
-                }).catch(err => {
-                    console.log(err);
-                })
-            }else {
-                e.target.innerHTML = originText;
-            }
-        };
+        // 如果任務進行中，就可以被修改
+        if(!e.target.previousSibling.previousSibling.checked) {
+            let originText = e.target.innerHTML;
+            let _id = e.target.getAttribute('data-id');
+            e.target.innerHTML = '<input type="text" id="updateInput" class="update-input-text"></input>';
+            var updateInput = document.getElementById('updateInput');
+            // 獲得焦點
+            updateInput.focus();   
+            // 失去焦點後，更新文本，並更新DB資料
+            updateInput.onblur = function () {
+                let updateVaule = updateInput.value;
+                if(updateVaule != "") {
+                    axios.post('/update-item', {
+                        text: updateVaule,
+                        id: _id
+                    }).then(function() {
+                        e.target.innerHTML = updateVaule;
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                }else {
+                    e.target.innerHTML = originText;
+                }
+            };
+        }
     }
 
     // 點擊checkbox 更改任務completed屬性真假值
